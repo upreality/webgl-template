6760bcd44b4b24e51f1a5c4e71134e132187449d
Denis Rebrov <denisrebrof@gmail.com>
2023-01-09 17:45:57 +0800

    separated settings

--- Assets/Scripts/Utils/WebSocketClient/data/WSSettings.cs
+++ Assets/Scripts/Utils/WebSocketClient/data/WSSettings.cs
@@ -1,16 +1,31 @@
-﻿using UnityEngine;
+﻿using System;
+using UnityEngine;
 
 namespace Utils.WebSocketClient.data
 {
     [CreateAssetMenu(menuName = "Settings/Web Socket Settings")]
     public class WSSettings : ScriptableObject
     {
+        [SerializeField] private ProtocolType protocol = ProtocolType.Secure;
         [SerializeField] private string host = "localhost";
         [SerializeField] private string port = "8080";
         [SerializeField] private string path = "websocket";
         public bool logConnection = true;
         public bool logMessages = true;
 
-        public string URL => $"wss://{host}:{port}/{path}";
+        public string URL => $"{Prefix}://{host}:{port}/{path}";
+
+        private string Prefix => protocol switch
+        {
+            ProtocolType.Secure => "wss",
+            ProtocolType.Unsecure => "ws",
+            _ => throw new ArgumentOutOfRangeException()
+        };
+
+        private enum ProtocolType
+        {
+            Secure,
+            Unsecure
+        }
     }
 }
